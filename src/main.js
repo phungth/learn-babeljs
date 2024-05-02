d0c81cce5b3eacf7f513709f7858fa9f8b294595
phungth <phungth@vng.com.vn>
2024-05-02 15:17:28 +0700

    feat: add examples

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 const obj = {
   arr: [1, 2, 3, 4],
-  printArr() {
-    console.log('iterableToArray', ...this.arr);
-  }
+  set: new Set([1, 2, 3]),
+  print() {
+    console.log("iterableToArray", ...this.arr);
+
+    console.log("stringLiteral", `array: ${this.arr.join(", ")}`);
+
+    const _set = Array.from(new Set([1, 2, 3, 2, 1]));
+    console.log("arrayFromSet", _set);
+  },
+  asyncFunction: async () => {
+    const promise = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(0);
+      }, 1000);
+    });
+    console.log("asyncFunction", (await promise) ?? 1);
+  },
 };
 
-obj.printArr();
+obj.print();
+obj.asyncFunction();
